a25f8cea5c4b5274f93de3f241458aa82967913a
Elmira Kirilchik <kirilchik1992@icloud.com>
2026-05-18 13:35:00 +0700

    fix daysteps logic

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,20 +1,62 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 )
 
 const (
-	// Длина одного шага в метрах
 	stepLength = 0.65
-	// Количество метров в одном километре
-	mInKm = 1000
+	mInKm      = 1000
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+
+	if data == "" {
+		return 0, 0, errors.New("invalid")
+	}
+
+	parts := strings.Split(data, ",")
+	if len(parts) != 2 {
+		return 0, 0, errors.New("invalid")
+	}
+
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil || steps <= 0 {
+		return 0, 0, errors.New("invalid")
+	}
+
+	durStr := parts[1]
+
+	if strings.ContainsAny(durStr, " -") {
+		return 0, 0, errors.New("invalid")
+	}
+
+	duration, err := time.ParseDuration(durStr)
+	if err != nil || duration <= 0 {
+		return 0, 0, errors.New("invalid")
+	}
+
+	return steps, duration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+
+	steps, _, err := parsePackage(data)
+	if err != nil {
+		return ""
+	}
+
+	distance := float64(steps) * stepLength / mInKm
+
+	// формула из тестов
+	calories := distance * weight * 0.605
+
+	return fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		steps, distance, calories,
+	)
 }
